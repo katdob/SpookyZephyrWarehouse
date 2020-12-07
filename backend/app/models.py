c852b7f7f5f60fa36934b5b08ed91e8b55863559
katdob <katdob@katdobs-MacBook-Air.local>
2020-12-06 21:04:06 -0500

    final adjustments and customizations to __init__ functions

--- backend/app/models.py
+++ backend/app/models.py
@@ -23,8 +23,8 @@ class Customer(db.Model):
         nullable=False)
 
     def __init__(self, full_name, contact_info_dict):
-        self.full_name = full_name
         
+        # check contact_info_dict
         key_list = ['phone_number', 'street_address', 'town/city', 'state_code', 'zip_code']
         for key in contact_info_dict:
             if key not in key_list:
@@ -33,7 +33,9 @@ class Customer(db.Model):
             if contact_info_dict[key] == '':
                 print('You must include key/value for {}.'.format(key))
                 return 'You must include key/value for {}.'.format(key)
+
         self.contact_info = contact_info_dict
+        self.full_name = full_name
 
     def __repr__(self):
         return '<Customer {}, {}>'.format(self.id, self.full_name)
@@ -48,7 +50,14 @@ class Order(db.Model):
     customer_id = Column(Integer, ForeignKey('customers.id'), nullable=False)
 
     def __init__(self, customer_id):
-        self.customer_id = customer_id
+        if not isinstance(customer_id, int):
+            return 'Your customer id must be an integer.'
+        
+        if not Customer.query.filter_by(id=customer_id).first():
+            return 'Your customer id must be the customer id of a customer already in the database.'
+        else:
+            self.customer_id = customer_id
+
         self.ordered_on = datetime.date.today()
 
     def __repr__(self):
@@ -65,10 +74,15 @@ class Product(db.Model):
     description = Column(String(500), index=True, nullable=False)
     suppliers = Column(PickleType, nullable=True) # a list of supplier ids (int)
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, suppliers):
+        if not suppliers or not isinstance(suppliers, list) or suppliers == []:
+            return 'You must provide a list of suppliers (int).'
+        for s in suppliers:
+            if not Supplier.query.filter_by(id=s).first():
+                return 'Supplier {} is not in the database yet. You must add this supplier before adding it to product {}.'.format(
+                    s, name)
         if description == '' or name == '':
-            print('You must include a name: {} and description: {}'.format(name, description))
-            # return 'You must include a name and description'
+             return 'You must include a name and and description.'
         self.name = name
         self.description = description
 
@@ -86,9 +100,32 @@ class Shipment(db.Model):
     supplier_id = Column(Integer, ForeignKey('suppliers.id'), nullable=False)
     order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
 
-    def __init__(self, products):
-        if not isinstance(products, list): 
-            return 'Products must be a list.'
+    def __init__(self, supplier_id, order_id, products):
+        
+        # check supplier_id
+        if not isinstance(supplier_id, int):
+            return 'Your supplier id must be an integer.'
+        if not Supplier.query.filter_by(id=supplier_id).first():
+            return 'Your supplier id must be a supplier id already in the database.'
+        else:
+            self.supplier_id = supplier_id
+        
+        # check order_id
+        if not isinstance(order_id, int):
+            return 'Your order id must be an integer.'
+        if not Order.query.filter_by(id=order_id).first():
+            return 'Your order id must be a order id already in the database.'
+        else:
+            self.order_id = order_id
+
+        # check products
+        if not isinstance(products, list) or products == []: 
+            return 'Products must be a list with at least one item.'
+        for p in products:
+            if not isinstance(p, int):  # if it's not an int, nope
+                return 'Each product must be a product id (int).'
+            if not Product.query.filter_by(id=p).first():  # if it's not a product, nope
+                return 'Each item in the product list must be a valid product already in the database.'
         self.products = products
 
     def __repr__(self):
@@ -111,11 +148,12 @@ class Supplier(db.Model):
             'zip_code': ''
         },
         nullable=False)
-    products = Column(PickleType, nullable=True) # a list of product ids (int)
 
     def __init__(self, name, contact_info_dict):
         self.name = name
         key_list = ['contact_name', 'phone_number', 'street_address', 'town/city', 'state_code', 'zip_code']
+        if key_list == [] or key_list == '' or not key_list:
+            return 'You must inclue contact information for a supplier.'
         for key in contact_info_dict:
             if key not in key_list:
                 return 'You must include key/value for {}.'
@@ -125,3 +163,5 @@ class Supplier(db.Model):
 
     def __repr__(self):
         return '<Supplier {}, {}>'.format(self.id, self.name)
+            
+
